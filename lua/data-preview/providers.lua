eae814b54142e1d99b112f11661dadff72a4000c
vinhhoanglong <longvinhvhbk@gmail.com>
2025-11-15 23:23:27 +0700

    basic code handler

--- lua/data-preview/providers.lua
+++ lua/data-preview/providers.lua
@@ -2,4 +2,49 @@
 -- implement head() file logic and get_metadata() logic of a parquet file
 
 local M = {}
+
+local data_command_providers = {
+	parquet = function(filepath)
+		if vim.fn.executable("parquet-tools") == 1 then
+			return { "sh", "-c", "parquet-tools csv '" .. filepath .. "' | head -n 21" }
+		else
+			return nil, "there is no parquet-tools"
+		end
+	end,
+}
+
+local stats_command_providers = {
+	parquet = function(filepath)
+		if vim.fn.executable("parquet-tools") == 1 then
+			return { "parquet-tools", "inspect", filepath }
+		else
+			return nil, "there is no parquet-tools"
+		end
+	end,
+}
+
+function M.get_command(filepath)
+	local ext = vim.fn.fnamemodify(filepath, ":e")
+	local provider_func = data_command_providers[ext]
+
+	if provider_func then
+		local cmd, err_msg = provider_func(filepath)
+		return cmd, err_msg
+	else
+		return nil, "This extension will be supported soon :) ... " .. ext
+	end
+end
+
+function M.get_stats_command(filepath)
+	local ext = vim.fn.fnamemodify(filepath, ":e")
+	local provider_func = stats_command_providers[ext]
+
+	if provider_func then
+		local cmd, err_msg = provider_func(filepath)
+		return cmd, err_msg
+	else
+		return nil, "This extension will be supported soon :) ... " .. ext
+	end
+end
+
 return M
